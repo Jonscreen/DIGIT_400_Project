4ae39c8ae9c9b55f079dbc17109c43a3914060ad
Jon Hayden <jayonathan42@gmail.com>
2019-03-14 21:23:06 +0000

    Initial Commit

---
+++ content.py
@@ -0,0 +1,13 @@
+def Content():
+    APP_CONTENT = {
+    "Home":[["Welcome", "/welcome/","Welcome to my awesome app!", "WideEyes.jpg"],
+           ["Background", "/background/", "Learn more about the app here!"],
+           ["Messages", "/messages/", "Your user messages are waiting..."],],
+    "Profile":[["User Profile", "/profile/", "Edit your profile here!", "WideEyes.jpg"],
+              ["Settings", "/settings/", "App Settins, no biggie."],
+              ["Terms of Sercive", "/tos/", "The legal stuff."],],
+    "Messages":[["Messsages", "/messages/", "Your user messages are waiting...", "WideEyes.jpg"],
+               ["Alerts", "/alerts/", "!!Urgent Alerts!!"],],
+    "Contact":[["Contact", "/contact/", "Contact us for support.", "WideEyes.jpg"],],
+    }
+    return APP_CONTENT
